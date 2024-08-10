e961cc7257afbed4b0e3d7ea78218b124488357d
Swapna-Bhairi <Swapna@iaminterested.co.in>
2024-08-10 23:44:07 +0530

    Enquiry form updated and services page modifications

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
-
+   // enquiry form buttons function 
+   selectedInterest: string = 'None';
+   interests = [
+     { value: '5', label: 'Marketing' },
+     { value: '4', label: 'Web Development' },
+     { value: '3', label: 'Graphic Design' },
+     { value: '2', label: 'Consulting' },
+     { value: '1', label: 'Influencer Marketing' }
+   ];
+ 
+   onInterestSelect(interest: string) {
+     this.selectedInterest = interest;
+   }
 }
