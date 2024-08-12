ebafea0d74aacbaed2b1dde9889b754acc494f5f
vamshi8267 <vamshigajangi009@gmail.com>
2024-08-12 09:58:17 +0530

    changed icons and changed content in home page

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about-us',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
-
+  faArrowRight = faArrowRight;
 }
