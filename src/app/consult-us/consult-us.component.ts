7c505561ca0b3abe4f7b9617d400af5fcbe65a36
Swapna-Bhairi <Swapna@iaminterested.co.in>
2024-08-29 18:54:44 +0530

    changes

--- src/app/consult-us/consult-us.component.ts
+++ src/app/consult-us/consult-us.component.ts
@@ -1,11 +1,16 @@
 import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-
+import { faMapMarkerAlt, faClock, faPhone } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-consult-us',
   templateUrl: './consult-us.component.html',
   styleUrls: ['./consult-us.component.css']
 })
 export class ConsultUsComponent {
+  faMapMarkerAlt = faMapMarkerAlt;
+  faClock = faClock;
+  faPhone = faPhone;
+
+
 }
